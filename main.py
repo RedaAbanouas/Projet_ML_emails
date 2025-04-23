0d3d85ef3b0f80f1dddb61dc9ff83a238a2c1db5
RedaAbanouas <abanouasreda15@gmail.com>
2025-04-23 23:35:48 +0100

    final

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ def predict():
 
     return jsonify({'prediction': pred_result})
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
